57af3fc61bbdf296d9ebf66106be709666a65c48
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2024-10-28 15:33:19 +0700

    "Removed .prettierignore file, refactored BottomNarbar component, and updated Header and index pages to include BottomNarbar"

--- src/components/BottomNarbar.tsx
+++ src/components/BottomNarbar.tsx
@@ -40,26 +40,35 @@ const navLinks = [
   },
 ]
 const BottomNarbar: React.FC = () => {
+  const mainRef = useRef(null)
+
   return (
-    <nav className="flex items-center">
-      <div className="glassmorphism md:bg-transparent md:dark:bg-transparent md:backdrop-blur-none md:static md:left-auto md:transform-none bg-bglight dark:bg-carddark dark:text-textlight md:drop-shadow-none fixed bottom-4 left-1/2 z-30 w-11/12 -translate-x-1/2 transform rounded drop-shadow-lg">
-        <div>
-          <ul className="flex items-center justify-evenly py-1">
-            {navLinks.map(navLink => (
-              <li key={navLink.url}>
-                <a
-                  href={navLink.url}
-                  className={`md:text-lg md:w-auto dark:fill-textlight md:mr-6 md:hover:text-marrsgreen md:dark:hover:text-carrigreen link-outline" } flex w-[4.5rem] flex-col items-center text-sm`}
-                >
-                  <span className="md:hidden">{navLink.svg}</span>
-                  <span className="whitespace-nowrap">{navLink.text}</span>
-                </a>
-              </li>
-            ))}
-          </ul>
+    <header className="md:flex">
+      <div
+        ref={mainRef}
+        className={`main-nav lower-glassmorphism bg-bglight dark:bg-bgdark duration-400 fixed top-0 z-30 h-16 w-full px-4 shadow-sm sm:px-8`}
+      >
+        <div className="mx-auto flex h-full w-full max-w-6xl items-center justify-between">
+          <nav className="flex items-center">
+            <div className="glassmorphism md:bg-transparent md:dark:bg-transparent md:backdrop-blur-none md:static md:left-auto md:transform-none bg-bglight dark:bg-carddark dark:text-textlight md:drop-shadow-none fixed bottom-4 left-1/2 z-30 w-11/12 -translate-x-1/2 transform rounded drop-shadow-lg">
+              <ul className="flex items-center justify-evenly py-1">
+                {navLinks.map(navLink => (
+                  <li key={navLink.url}>
+                    <a
+                      href={navLink.url}
+                      className={`md:text-lg md:w-auto dark:fill-textlight md:mr-6 md:hover:text-marrsgreen md:dark:hover:text-carrigreen link-outline flex w-[4.5rem] flex-col items-center text-sm`}
+                    >
+                      <span className="md:hidden">{navLink.svg}</span>
+                      <span className="whitespace-nowrap">{navLink.text}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </nav>
         </div>
       </div>
-    </nav>
+    </header>
   )
 }
 
