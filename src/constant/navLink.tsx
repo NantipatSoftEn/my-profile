fea77dad91646ec9578be2369168eb175eb51283
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2024-11-03 23:01:24 +0700

    update

--- src/constant/navLink.tsx
+++ src/constant/navLink.tsx
@@ -4,7 +4,7 @@
 
 export const navLinks = [
   {
-    url: '#whoami',
+    url: '/about',
     svg: (
       <svg
         width="800px"
@@ -12,7 +12,7 @@ export const navLinks = [
         viewBox="0 0 64 64"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g fill="none" fill-rule="evenodd">
+        <g fill="none" fillRule="evenodd">
           <path
             fill="#9C4D4D"
             d="M11,7.99079514 C11,6.89130934 11.514871,6.73552996 12.1456788,7.63668394 L25,26 L11,26 L11,7.99079514 Z"
@@ -33,13 +33,13 @@ export const navLinks = [
           <circle cx="42" cy="36" r="2" fill="#595959" />
           <path
             stroke="#595959"
-            stroke-linecap="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeWidth="2"
             d="M30.8154771 50C30.8154771 50 27.6116078 52.9001474 24.4077385 52.9001474 21.2038693 52.9001474 18 50 18 50M43.8154771 50C43.8154771 50 40.6116078 52.9001474 37.4077385 52.9001474 34.2038693 52.9001474 31 50 31 50"
           />
           <path
             stroke="#9C4D4D"
-            stroke-linecap="round"
+            strokeLinecap="round"
             d="M3 41L22.0815162 45.8675256M3.5 37.5L22.0815162 44.8675256M43 44.270357L61.5 39.5M43 45.270357L61.5 42.5"
           />
         </g>
@@ -57,7 +57,7 @@ export const navLinks = [
     text: 'Projects',
   },
   {
-    url: '#blog',
+    url: '/post',
     svg: (
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
         <path d="M19.875 3H4.125C2.953 3 2 3.897 2 5v14c0 1.103.953 2 2.125 2h15.75C21.047 21 22 20.103 22 19V5c0-1.103-.953-2-2.125-2zm0 16H4.125c-.057 0-.096-.016-.113-.016-.007 0-.011.002-.012.008L3.988 5.046c.007-.01.052-.046.137-.046h15.75c.079.001.122.028.125.008l.012 13.946c-.007.01-.052.046-.137.046z" />
@@ -67,7 +67,7 @@ export const navLinks = [
     text: 'Blog',
   },
   {
-    url: '#contact',
+    url: '/notes',
     svg: (
       <svg width="24px" height="24px" xmlns="http://www.w3.org/2000/svg">
         <title />
@@ -103,6 +103,6 @@ export const navLinks = [
         <circle cx="19" cy="9.75" r="0.75" style={{ fill: '#fff' }} />
       </svg>
     ),
-    text: 'Contact',
+    text: 'Note',
   },
 ]
